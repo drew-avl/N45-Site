78406c2301bc8d1b34854b10e2299523c3c9e557
Drew Hamilton <drew.hamilton.avl@gmail.com>
2026-08-20 14:53:17 -0400

    Add N45 Field Notes blog route

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        blog: "./blog/index.html",
+      },
+    },
+  },
   css: {
     transformer: "lightningcss",
   },
